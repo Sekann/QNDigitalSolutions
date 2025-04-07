ccc27a79b39827034f31b916476688edfc93a882
drodriguez5503 <davidrdelope@gmail.com>
2025-04-07 10:39:54 +0200

    Creados componentes principales de cliente y backoffice

---
+++ QnEventos/src/app/cliente/parcels/parcels.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-parcels',
+  standalone: true,
+  imports: [],
+  templateUrl: './parcels.component.html',
+  styleUrl: './parcels.component.scss'
+})
+export class ParcelsComponent {
+
+}
