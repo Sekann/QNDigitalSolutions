807c148794ecdf5256d20b728ea82e1607659fc7
Your Name <you@example.com>
2025-04-07 11:29:51 +0200

    rutas y componente layout

--- QnEventos/src/app/app.routes.ts
+++ QnEventos/src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { LayoutComponent } from './cliente/layout/layout.component';
+import { VisitorsComponent } from './cliente/visitors/visitors.component';
+import { BillsComponent } from './cliente/bills/bills.component';
+import { ParcelsComponent } from './cliente/parcels/parcels.component';
+import { ServicesComponent } from './cliente/services/services.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    {
+        path: "", component: LayoutComponent, children: [
+          {path: "visitors", component: VisitorsComponent },
+          {path:"bills", component: BillsComponent},
+          {path: "parcels", component: ParcelsComponent },
+          {path: "services", component: ServicesComponent },
+        ]
+      }
+
+];
