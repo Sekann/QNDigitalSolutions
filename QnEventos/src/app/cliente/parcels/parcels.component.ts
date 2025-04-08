ea3e71cf031c6e824d7b0d1be66bee4d92cde1fd
drodriguez5503 <davidrdelope@gmail.com>
2025-04-08 14:19:59 +0200

    Seccion Parcels hecha

--- QnEventos/src/app/cliente/parcels/parcels.component.ts
+++ QnEventos/src/app/cliente/parcels/parcels.component.ts
@@ -1,12 +1,71 @@
 import { Component } from '@angular/core';
+import {RouterLink} from "@angular/router";
+import {NgClass, NgForOf, NgIf} from "@angular/common";
 
 @Component({
   selector: 'app-parcels',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterLink,
+    NgForOf,
+    NgIf,
+    NgClass
+  ],
   templateUrl: './parcels.component.html',
   styleUrl: './parcels.component.scss'
 })
 export class ParcelsComponent {
+  confirmModal : boolean = false;
+  selectedParcel : any
+  parcels  = [
+    {received: false,
+      id: "PC1472589B",
+      date: "9 Jan, 2021",
+      time: "11:42",
+      location: "DHL, Parcel",
+      type: "box"},
+    {received: false,
+      id: "RM305084A",
+      date: "9 Jan, 2021",
+      time: "11:03",
+      location: "DHL, Parcel",
+      type:"letter"},
+    {received: false,
+      id: "PC1472589B",
+      date: "9 Jan, 2021",
+      time: "10:42",
+      location: "DHL, Parcel",
+      type:"box"},
+    {received: true,
+      id: "PC1472589B",
+      date: "8 Jan, 2021",
+      time: "14:08",
+      location: "DHL, Parcel",
+      type:"box"},
+    {received: true,
+      id: "RM305084A",
+      date: "8 Jan, 2021",
+      time: "10:18",
+      location: "DHL, Parcel",
+      type:"letter"},
+  ]
+
+  noParcelsToReceive(): boolean {
+    return this.parcels.filter(parcel => !parcel.received).length === 0;
+  }
+
+  changeToReceived(parcel:any){
+    parcel.received = true;
+    this.confirmModal = false;
+  }
+
+  openConfirmModal(parcel:any){
+    this.selectedParcel = parcel;
+    this.confirmModal = true;
+  }
+
+  closeConfirm() {
+    this.confirmModal = false;
+  }
 
 }
