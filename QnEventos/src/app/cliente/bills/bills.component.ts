f289e14b720aadec2a3092e70b6a4ad7ffa751ab
drodriguez5503 <davidrdelope@gmail.com>
2025-04-08 11:06:37 +0200

    Estructura inicial bills.component

--- QnEventos/src/app/cliente/bills/bills.component.ts
+++ QnEventos/src/app/cliente/bills/bills.component.ts
@@ -1,12 +1,51 @@
 import { Component } from '@angular/core';
+import {NgClass, NgForOf, NgIf} from "@angular/common";
+import {RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-bills',
   standalone: true,
-  imports: [],
+  imports: [
+    NgClass,
+    NgIf,
+    NgForOf,
+    RouterLink
+  ],
   templateUrl: './bills.component.html',
   styleUrl: './bills.component.scss'
 })
 export class BillsComponent {
+  billsGrouped = [
+    {month: 'January, 2021', total: 142, items: [
+      { title: 'Maintenance fee',
+          amount: 69,
+          date: '5 Jan, 2021',
+          paid: true,
+          icon: 'bi-receipt',
+          iconClass: 'bg-gradient-teal'
+        }, { title: 'Electricity bill',
+          amount: 73,
+          date: '2 Jan, 2021',
+          paid: true,
+          icon: 'bi-lightning-charge-fill',
+          iconClass: 'bg-gradient-orange'
+        }]},
+    {month: 'December, 2020', total: 136, items: [
+        {title: 'Maintenance fee',
+          amount: 69,
+          date: '5 Dec, 2020',
+          paid: false,
+          icon: 'bi-receipt',
+          iconClass: 'bg-gradient-teal'
+        }, {title: 'Electricity bill',
+          amount: 67,
+          date: '2 Dec, 2020',
+          paid: false,
+          icon: 'bi-lightning-charge-fill',
+          iconClass: 'bg-gradient-orange'
+        }
+      ]
+    }
+  ];
 
 }
