18b97fbaea010442c238ff40f3bcc6af30441af2
drodriguez5503 <davidrdelope@gmail.com>
2025-04-08 16:34:18 +0200

    Empezado componente services

--- QnEventos/src/app/cliente/services/services.component.ts
+++ QnEventos/src/app/cliente/services/services.component.ts
@@ -1,12 +1,43 @@
 import { Component } from '@angular/core';
+import {RouterLink} from "@angular/router";
+import {NgForOf} from "@angular/common";
 
 @Component({
   selector: 'app-services',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterLink,
+    NgForOf
+  ],
   templateUrl: './services.component.html',
   styleUrl: './services.component.scss'
 })
 export class ServicesComponent {
+  services = [
+    {title: 'Premium cleaning',
+      price: 129,
+      image: '/img.png',
+    },
+    {title: 'Standard cleaning',
+      price: 79,
+      image: '/img.png'
+    },
+    {title: 'Wash & Dry',
+      price: 39,
+      image: '/img.png'
+    },
+    {title: 'Electricity',
+      price: 99,
+      image: '/img.png'
+    },
+    {title: 'Car services',
+      price: 159,
+      image: '/img.png'
+    },
+    {title: 'Moving services',
+      price: 89,
+      image: '/img.png'
+    }
+  ];
 
 }
